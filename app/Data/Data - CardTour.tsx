1410f22487df195fd176a6711c9f1cae32f41637
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-05 08:19:23 -0400

    Datos estaticos de la pagina

---
+++ app/Data/Data - CardTour.tsx
@@ -0,0 +1,31 @@
+
+export const DataCardsTour = [
+    {
+        key:1,
+        Img: 'https://i.pinimg.com/736x/ec/4a/19/ec4a19a40d2b9d7e30219ea7fabde2ad.jpg',
+        Title: 'Alone with nature',
+        Start: 5,
+        Price: '99',
+    },
+    {
+        key:2,
+        Img: 'https://i.pinimg.com/736x/63/64/ff/6364ff2dafaa8833d5301e862e4b9331.jpg',
+        Title: 'Alone with nature',
+        Start: 5,
+        Price: '200',
+    },
+    {
+        key:3,
+        Img: 'https://i.pinimg.com/736x/eb/da/a5/ebdaa5286e6072563651bac6a74da020.jpg',
+        Title: 'Alone with nature',
+        Start: 5,
+        Price: '100',
+    },
+    {
+        key:4,
+        Img: 'https://i.pinimg.com/736x/7a/1f/16/7a1f16f4cb34d059bbbcdcf1e734e7ea.jpg',
+        Title: 'Alone with nature',
+        Start: 5,
+        Price: '40',
+    }
+]
