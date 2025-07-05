d9eda52f892966066353471d4a614048657b843f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-05 08:19:14 -0400

    Datos estaticos de la pagina

---
+++ app/Data/Data - Card.tsx
@@ -0,0 +1,35 @@
+import { BsSend } from "react-icons/bs";
+import { BsWallet } from "react-icons/bs";
+import { HiOutlineUserGroup } from "react-icons/hi2";
+import { LuBadgePercent } from "react-icons/lu";
+
+export const DataCard = [
+    {
+        key:1,
+        Icon: <BsSend className="text-xl"/>,
+        Title: 'Airport pickup',
+        Description: `Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum.`,
+    },
+    {
+        key:1,
+        Icon: <BsWallet className="text-xl"/>,
+        Title: 'Easy booking',
+        Description: `Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum.`,
+    },
+    {
+        key:1,
+        Icon: <HiOutlineUserGroup className="text-xl"/>,
+        Title: 'Best tour guide',
+        Description: `Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum.`,
+    },
+    {
+        key:1,
+        Icon: <LuBadgePercent className="text-xl"/>,
+        Title: 'Lots',
+        Description: `Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum.`,
+    }
+]
